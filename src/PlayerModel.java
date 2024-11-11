2adbc452ae3fba68008116917d3eeecf8b2d92a2
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2024-11-11 19:26:04 +0700

    Initialize Java project structure with basic files and configurations

---
+++ src/PlayerModel.java
@@ -0,0 +1,14 @@
+
+import java.util.List;
+
+public class PlayerModel {
+
+    private String playerName;
+    private int playerScore;
+    private int playerEnergy;
+    private int playerPosRow;
+    private int playerPosCol;
+    private boolean playerIsAlive;
+    private List<String> playerMovementList;
+
+}
